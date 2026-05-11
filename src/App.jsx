1d8f416d5a508d5f9c9e646b8344c7735a6a6225
pallvviidhiman-IT <pallvviidhiman@gmail.com>
2026-05-11 10:50:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react";
 
 function App() {
+
   const [users, setUsers] = useState([]);
+
   const [name, setName] = useState("");
+
+  const [editId, setEditId] =
+    useState(null);
+
 
   useEffect(() => {
     getUsers();
   }, []);
+
 
   function getUsers() {
     fetch(
@@ -16,7 +23,9 @@ function App() {
       .then((data) => setUsers(data));
   }
 
+
   function addUser() {
+
     fetch(
       `${import.meta.env.VITE_API_URL}/users`,
       {
@@ -33,14 +42,69 @@ function App() {
     )
       .then((res) => res.json())
       .then(() => {
+
         getUsers();
 
         setName("");
       });
   }
 
+
+  function deleteUser(id) {
+
+    fetch(
+      `${import.meta.env.VITE_API_URL}/users/${id}`,
+      {
+        method: "DELETE"
+      }
+    )
+      .then((res) => res.json())
+      .then(() => {
+
+        getUsers();
+      });
+  }
+
+
+  function editUser(user) {
+
+    setName(user.name);
+
+    setEditId(user._id);
+  }
+
+
+  function updateUser() {
+
+    fetch(
+      `${import.meta.env.VITE_API_URL}/users/${editId}`,
+      {
+        method: "PUT",
+
+        headers: {
+          "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+          name: name
+        })
+      }
+    )
+      .then((res) => res.json())
+      .then(() => {
+
+        getUsers();
+
+        setName("");
+
+        setEditId(null);
+      });
+  }
+
+
   return (
     <div>
+
       <h1>User App</h1>
 
       <input
@@ -51,15 +115,44 @@ function App() {
         }
       />
 
-      <button onClick={addUser}>
-        Add User
-      </button>
+      {
+        editId ? (
+          <button onClick={updateUser}>
+            Update User
+          </button>
+        ) : (
+          <button onClick={addUser}>
+            Add User
+          </button>
+        )
+      }
 
-      {users.map((user) => (
-        <h2 key={user._id}>
-          {user.name}
-        </h2>
-      ))}
+      {
+        users.map((user) => (
+          <div key={user._id}>
+
+            <h2>{user.name}</h2>
+
+            <button
+              onClick={() =>
+                editUser(user)
+              }
+            >
+              Edit
+            </button>
+
+            <button
+              onClick={() =>
+                deleteUser(user._id)
+              }
+            >
+              Delete
+            </button>
+
+          </div>
+        ))
+      }
+
     </div>
   );
 }
